8a08f84e7cadf2f5e989398eb7c0afeb9df71003
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-05-07 15:45:31 +0700

    membuat class mahasiswa dan variabel pada pointer 1

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -9,16 +9,3 @@ public:
 	}
 };
 
-int main() {
-	mahasiswa mhs{ 1 }; // object mhs
-	mhs.showNim(); //member acces operator
-
-	mahasiswa& refMhs = mhs; //Pointer reference refMhs
-	refMhs.nim = 2; // member access operator
-	mhs.showNim();
-
-	mahasiswa* pmhs = &mhs; //pointer dereference pMhs
-	pmhs->nim = 3; //Arrow operator
-	pmhs->showNim();
-	return 0;
-}
